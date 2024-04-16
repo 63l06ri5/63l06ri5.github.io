5da739a61faa90b48173777f585531e5a09a5252
Ilya Turkin <63l06ri5@gmail.com>
2024-04-16 18:18:23 +0300

    in da middle

--- pdflib/index.js
+++ pdflib/index.js
@@ -1,62 +1,152 @@
-// @ts-check
-
-const PDFDocument = PDFLib.PDFDocument;
-
-async function splitPdf() {
-  const docmentAsBytes = await document
-    .querySelector("#pdfinput")
-    .files[0].arrayBuffer();
-
-  // Load your PDFDocument
-  const pdfDoc = await PDFDocument.load(docmentAsBytes);
-
-  const subDocument = await PDFDocument.create();
-
-  const idxs = document
-    .querySelector("#pages")
-    .value.split(/[\s,]/)
-    .map((x) => parseInt(x) - 1);
-  console.log(idxs);
-
-  const copiedPages = await subDocument.copyPages(pdfDoc, idxs);
-
-  for (const copiedPage of copiedPages) {
-    subDocument.addPage(copiedPage);
-  }
-  const pdfBytes = await subDocument.save();
-  await downloadFileThroughATagWithBlob(pdfBytes, `file.pdf`);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a;
+function splitPdf() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!pdfFile) {
+            return;
+        }
+        // Load your PDFDocument
+        const pdfLibDoc = yield window.PDFLib.PDFDocument.load(pdfFile);
+        const subDocument = yield window.PDFLib.PDFDocument.create();
+        const idxsRaw = document.querySelector("#pages").value.split(/[\s,]/);
+        const idxs = [];
+        idxsRaw.forEach((x) => {
+            const matches = x.match(/^(\d+)(\-(\d+))?$/);
+            if (!matches) {
+                return;
+            }
+            const leftMatch = parseInt(matches[1]) - 1;
+            if (leftMatch < 0 || leftMatch >= pdfDoc.numPages) {
+                return;
+            }
+            if (matches[3]) {
+                const rightMatch = parseInt(matches[3]) - 1;
+                if (rightMatch < 0 ||
+                    rightMatch <= leftMatch ||
+                    rightMatch >= pdfDoc.numPages) {
+                    return;
+                }
+                idxs.push(...arrayRange(leftMatch, rightMatch, 1));
+            }
+            else {
+                idxs.push(leftMatch);
+            }
+        });
+        if (idxs.length == 0) {
+            return;
+        }
+        console.log(idxs);
+        const copiedPages = yield subDocument.copyPages(pdfLibDoc, idxs);
+        for (const copiedPage of copiedPages) {
+            subDocument.addPage(copiedPage);
+        }
+        const pdfBytes = yield subDocument.save();
+        yield downloadFileThroughATagWithBlob(pdfBytes, `file.pdf`);
+    });
 }
-
-
-
-
-/**
- *
- * @param {Uint8Array} blob
- * @param {string} fileName
- */
 function downloadFileThroughATagWithBlob(blob, fileName) {
-  const url = window.URL.createObjectURL(new Blob([blob]));
-
-  downloadFileThroughATagWithUrl(url, fileName);
-  setTimeout(() => window.URL.revokeObjectURL(url), 60000);
+    const url = window.URL.createObjectURL(new Blob([blob]));
+    downloadFileThroughATagWithUrl(url, fileName);
+    setTimeout(() => window.URL.revokeObjectURL(url), 60000);
 }
-
-/**
- *
- * @param {string} objectUrl
- * @param {string} fileName
- */
 function downloadFileThroughATagWithUrl(objectUrl, fileName) {
-  const a = document.createElement("a");
-  a.style = "display: none";
-  document.body.appendChild(a);
-
-  a.href = objectUrl;
-  a.download = fileName;
-  a.click();
-
-  document.body.removeChild(a);
+    const a = document.createElement("a");
+    a.style.cssText = "display: none";
+    document.body.appendChild(a);
+    a.href = objectUrl;
+    a.download = fileName;
+    a.click();
+    document.body.removeChild(a);
 }
-
 document.querySelector("#button").addEventListener("click", splitPdf);
+var pdfDoc = null, pdfFile = undefined, pageNum = 1, pageRendering = false, pageNumPending = null, scale = 0.8, canvas = document.getElementById("the-canvas"), ctx = canvas.getContext("2d");
+/**
+ * Get page info from document, resize canvas accordingly, and render page.
+ * @param num Page number.
+ */
+function renderPage(num) {
+    pageRendering = true;
+    // Using promise to fetch the page
+    pdfDoc.getPage(num).then(function (page) {
+        var viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        // Render PDF page into canvas context
+        var renderContext = {
+            canvasContext: ctx,
+            viewport: viewport,
+        };
+        var renderTask = page.render(renderContext);
+        // Wait for rendering to finish
+        renderTask.promise.then(function () {
+            pageRendering = false;
+            if (pageNumPending !== null) {
+                // New page rendering is pending
+                renderPage(pageNumPending);
+                pageNumPending = null;
+            }
+        });
+    });
+    // Update page counters
+    document.getElementById("page_num").textContent =
+        num.toString();
+}
+/**
+ * If another page rendering in progress, waits until the rendering is
+ * finised. Otherwise, executes rendering immediately.
+ */
+function queueRenderPage(num) {
+    if (pageRendering) {
+        pageNumPending = num;
+    }
+    else {
+        renderPage(num);
+    }
+}
+/**
+ * Displays previous page.
+ */
+function onPrevPage() {
+    if (pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+document.getElementById("prev").addEventListener("click", onPrevPage);
+/**
+ * Displays next page.
+ */
+function onNextPage() {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+document.getElementById("next").addEventListener("click", onNextPage);
+(_a = document.getElementById("pdfinput")) === null || _a === void 0 ? void 0 : _a.addEventListener("change", loadFile);
+function loadFile() {
+    return __awaiter(this, void 0, void 0, function* () {
+        pdfFile = yield document.querySelector("#pdfinput").files[0].arrayBuffer();
+        /**
+         * Asynchronously downloads PDF.
+         */
+        pdfjsLib.getDocument(pdfFile.slice()).promise.then(function (pdfDoc_) {
+            pdfDoc = pdfDoc_;
+            document.getElementById("page_count").textContent = pdfDoc.numPages;
+            // Initial/first page rendering
+            renderPage((pageNum = 1));
+        });
+    });
+}
+const arrayRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (value, index) => start + index * step);
